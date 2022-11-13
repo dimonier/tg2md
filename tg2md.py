b7fb3d0fca79e417e27cfd33e03b7e69212964bb
Дмитрий Уланов <dnulanov@sberbank.ru>
2022-11-13 21:04:59 +0300

    Added post author to header

--- tg2md.py
+++ tg2md.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
-# parse.py - converts telegram json to jekyll md.
+# parse.py - converts telegram json to Obsidian md.
 # Copyright (c) 2020, Lev Brekalov
+# Changes from progxaker, 2021
 
 # TODO summary:
 # - replies
 # - single/muliple tags
 # - forwarded posts
 # - custom post header
-# - multiple photos in one post
 
 import os
-import logging
 import argparse
 import json
 from datetime import datetime
 
+def print_default_post_header(post_title, post_date, post_author, post_tag):
 
-def print_default_post_header(post_title, post_date, post_tag):
 
     '''
     returns default post header
@@ -28,9 +27,10 @@ def print_default_post_header(post_title, post_date, post_tag):
     post_header = '---\n'\
         'title: {title}\n'\
         'date: {date}\n'\
+        'author: {author}\n'\
         'tags: {tag}\n'\
         'layout: post\n'\
-        '---\n'.format(title=post_title, date=post_date, tag=post_tag)
+        '---\n'.format(title=post_title, date=post_date, author=post_author, tag=post_tag)
 
     return post_header
 
@@ -54,33 +54,9 @@ def parse_post_photo(post, photo_dir):
     converts photo tag to markdown image link
     '''
 
-    post_photo_src = os.path.basename(post['photo'])
-    post_photo_src = os.path.join(photo_dir, post_photo_src)
-    post_photo = '![image]({src})\n\n'.format(src=post_photo_src)
+    post_photo = '![image]({src})\n\n'.format(src=post['photo'])
 
     return post_photo
-
-
-def parse_post_photo_as_file(post, media_dir):
-
-    '''
-    converts file tag with thumbnail to image and a link
-    '''
-
-    # links to files are currently broken, because these files are
-    # going to `files` directory, not `photos`.
-    # need to track down any files with thumbnails and then to move them
-    # to a photos directory.
-    post_photo_file_src = os.path.basename(post['file'])
-    post_photo_file_src = os.path.join(media_dir, post_photo_file_src)
-    post_photo_thumbnail_src = os.path.basename(post['thumbnail'])
-    post_photo_thumbnail_src = os.path.join(media_dir,
-                                            post_photo_thumbnail_src)
-
-    post_photo_as_file = '![image]({thumb})\n[full size]({file})\n\n'\
-        .format(thumb=post_photo_thumbnail_src, file=post_photo_file_src)
-
-    return post_photo_as_file
 
 
 def text_format(string, fmt):
@@ -183,36 +159,13 @@ def parse_post_text(post):
 def parse_post_media(post, media_dir):
 
     '''
-    wraps media files into html tags
+    wraps file links to Obsidian link
     '''
 
-    post_media_file = os.path.basename(post['file'])
-    post_media_ext = post_media_file.split(".")[-1]
-    post_media_src = os.path.join(media_dir, post_media_file)
-
-    # audiofiles can be presented as audioplayers and other media types
-    # could be left as just links to them
-    # ???
-    post_media = '\n<audio controls>\n\
-            <source src="{src}" type="{mime_type}">\n\
-            </audio>'.format(src=post_media_src, mime_type=post['mime_type'])
+    post_media = '\n![[{src}]]'.format(src=post['file'])
 
     return post_media
 
-
-def parse_post_file(post, media_dir):
-
-    '''
-    wrap files into link tags
-    '''
-
-    post_file_src = os.path.basename(post['file'])
-    post_file_ext = post_file_src.split('.')[-1]
-    post_file_name = post_file_src.removesuffix('.' + post_file_ext)
-
-    post_file = f'\n\n[{post_file_name}]({post_file_src})\n\n'
-
-    return post_file
 
 def parse_post(post, photo_dir, media_dir):
 
@@ -223,12 +176,8 @@ def parse_post(post, photo_dir, media_dir):
     post_output = ''
 
     # optional image
-    # TODO: handle multiple photos in one post (maybe by comparing timestamps)
     if 'photo' in post:
         post_output += str(parse_post_photo(post, photo_dir))
-
-    if all(['file' in post, 'thumbnail' in post]):
-        post_output += str(parse_post_photo_as_file(post, media_dir))
 
     # post text
     post_output += str(parse_post_text(post))
@@ -236,8 +185,6 @@ def parse_post(post, photo_dir, media_dir):
     # optional media
     if 'media_type' in post:
         post_output += str(parse_post_media(post, media_dir))
-    elif 'file' in post and not 'thumbnail' in post:
-        post_output += str(parse_post_file(post, media_dir))
 
     return post_output
 
@@ -247,30 +194,30 @@ def main():
     parser = argparse.ArgumentParser(
             usage='%(prog)s [options] json_file',
             description='Convert exported Telegram channel data json to \
-                    bunch of markdown posts ready to use with jekyll')
+                    bunch of markdown posts ready to use with Obsidian')
     parser.add_argument(
             'json', metavar='json_file',
             help='result.json file from telegram export')
     parser.add_argument(
-            '-o', '--out-dir', metavar='out_dir',
+            '--out-dir', metavar='out_dir',
             nargs='?', default='formatted_posts',
             help='output directory for markdown files\
                     (default: formatted_posts)')
     parser.add_argument(
-            '-p', '--photo-dir', metavar='photo_dir',
+            '--photo-dir', metavar='photo_dir',
             nargs='?', default='photos',
             help='location of image files. this changes only links\
                     to photos in markdown text, so specify your\
                     desired location (default: photos)')
     parser.add_argument(
-            '-m', '--media-dir', metavar='media_dir',
+            '--media-dir', metavar='media_dir',
             nargs='?', default='files',
             help='location of media files. this changes only links\
                     to files in markdown text, so specify your \
                     desired location (default: files)')
     args_wip = parser.add_argument_group('work in progress')
     args_wip.add_argument(
-            '-H', '--post-header', metavar='post_header',
+            '--post-header', metavar='post_header',
             nargs='?',
             help='yaml front matter for your posts \
                     (now doesn\'t work)')
@@ -279,8 +226,8 @@ def main():
 
     try:
         os.mkdir(args.out_dir)
-    except OSError as error:
-        logging.warning(error)
+    except FileExistsError:
+        pass
 
     # load json file
     try:
@@ -298,12 +245,13 @@ def main():
 
             post_date = datetime.fromisoformat(post['date'])
             post_id = post['id']
+            post_author = post['from']
             post_filename = str(post_date.date()) + '-' + str(post_id) + '.md'
             post_path = os.path.join(args.out_dir, post_filename)
 
             with open(post_path, 'w', encoding='utf-8') as f:
-                print(print_default_post_header(post_id, post_date, None),
-                      file=f)
+                print(print_default_post_header(
+                    post_id, post_date, post_author, None), file=f)
                 print(parse_post(post, args.photo_dir, args.media_dir), file=f)
 
 
